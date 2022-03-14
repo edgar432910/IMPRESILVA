9f487fdc0d9b74e3cbdcd41b204fff8d05c07a52
Edgar_Rodriguez <edgar432910@hotmail.com>
2022-03-14 09:33:16 -0500

    a

--- clientSistema_Consulta/src/app/_service/guard.service.ts
+++ clientSistema_Consulta/src/app/_service/guard.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginService } from './login.service';
+import { MenuService } from './menu.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GuardService {
+ 
 
-  constructor() { }
+  constructor( private loginService: LoginService,
+    private menuService: MenuService,
+    private router: Router) { }
+
+  
+  
+    
 }
